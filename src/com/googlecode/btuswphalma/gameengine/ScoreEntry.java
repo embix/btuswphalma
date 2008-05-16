a2fc752a7f4a601863b763098061e17e2719d71e
chke85 <chke85@1e8899dc-0e4b-0410-8c4f-1f3d18b41dae>
2008-05-16 17:55:42 +0000

    kleine formataenderungen

--- src/com/googlecode/btuswphalma/gameengine/ScoreEntry.java
+++ src/com/googlecode/btuswphalma/gameengine/ScoreEntry.java
@@ -1,49 +1,56 @@
 /** 
  * ScoreEntry.java
+ * 
  * Beinhaltet einen Spielwertungseintrag (score entry) mit den Werten:
  * Spielername, Spielerwertung (Rang) und benoetigte Runden
+ * 
+ * @author Christoph
  */
 package com.googlecode.btuswphalma.gameengine;
 
 public class ScoreEntry {
-	
-	private	int ranking;
-	private	String name;
-	private	int rounds;
-	
+
+	private int ranking;
+
+	private String name;
+
+	private int rounds;
+
 	/* der Konstruktor ScoreEntry der mit festen Parametern initialisiert wird */
 	public ScoreEntry(int rank, String spname, int runden) {
 		this.ranking = rank;
 		this.name = spname;
 		this.rounds = runden;
 	}
-	
+
 	/* Ausgabe des Ranges dieses Eintrages */
 	public int getRanking() {
 		return this.ranking;
 	}
-	
+
 	/* Ausgabe des Spielernamens dieses Eintrages */
 	public String getName() {
 		return this.name;
 	}
-	
+
 	/* Ausgabe der Runden die der Spieler gebraucht hat (aktueller Eintrag) */
 	public int getRounds() {
 		return this.rounds;
 	}
-	
-	/* Setz-Methoden die den aktuellen Eintrag nach dessen Erstellung veraendern koennen
-	 * Verwendung wahrscheinlich nicht noetig
-	public void setRanking(int rank) {
-		this.ranking = rank;
-	}
 
-	public void setName(String spname) {
-		this.name = spname;
-	}
-	
-	public void setRounds(int runden) {
-		this.rounds = runden;
-	}*/
+	/*
+	 * Setz-Methoden die den aktuellen Eintrag nach dessen Erstellung veraendern
+	 * koennen 
+	 * Verwendung wahrscheinlich nicht noetig 
+	 public void setRanking(int rank) { 
+	 	this.ranking = rank; 
+	 }
+	 
+	 public void setName(String spname) { 
+	 	this.name = spname; 
+	 }
+	  
+	 public void setRounds(int runden) { 
+	 	this.rounds = runden; 
+	 }*/
 }
--- src/com/googlecode/btuswphalma/gameengine/ScoreList.java
+++ src/com/googlecode/btuswphalma/gameengine/ScoreList.java
@@ -1,6 +1,9 @@
 /**
  * ScoreList.java
+ * 
  * Beinhaltet die Spielerwertungsliste die aus bis zu 6 Eintraegen besteht
+ * 
+ * @author Christoph
  */
 package com.googlecode.btuswphalma.gameengine;
 
@@ -11,25 +14,32 @@ public class ScoreList {
 	/* erzeugen einer ArrayList */
 	private ArrayList<ScoreEntry> scores;
 
-	/* initialisieren der Spielerwertungsliste, dessen Groeße in Abhaengigkeit der Spieleranzahl */
+	/*
+	 * initialisieren der Spielerwertungsliste, dessen Groeße in Abhaengigkeit
+	 * der Spieleranzahl
+	 */
 	public ScoreList(int anz) {
 		this.scores = new ArrayList<ScoreEntry>(anz);
 	}
-	
+
 	/* gibt die komplette Spielerwertungsliste aus */
 	public ArrayList<ScoreEntry> getScoreList() {
 		return this.scores;
 	}
-	
-	/* gibt einen Eintrag aus der Liste aus, anhand des Ranges 
-	 * (dieser muss inkrementiert werden)*/
+
+	/*
+	 * gibt einen Eintrag aus der Liste aus, anhand des Ranges (dieser muss
+	 * inkrementiert werden)
+	 */
 	public ScoreEntry getEntry(int rank) {
 		ScoreEntry entry = scores.get(rank + 1);
 		return entry;
 	}
-	
-	/* fuegt einen Eintrag in die Liste hinzu, dabei bestimmt der Rang des 
-	 * Eintrages die Position des Eintrages in der Liste*/
+
+	/*
+	 * fuegt einen Eintrag in die Liste hinzu, dabei bestimmt der Rang des
+	 * Eintrages die Position des Eintrages in der Liste
+	 */
 	public void addEntry(ScoreEntry entry) {
 		int rank = entry.getRanking() - 1;
 		scores.add(rank, entry);
