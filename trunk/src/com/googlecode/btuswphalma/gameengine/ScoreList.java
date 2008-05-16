f5f66b63333d8bfc8f9309ac87dd42a710c8da8b
chke85 <chke85@1e8899dc-0e4b-0410-8c4f-1f3d18b41dae>
2008-05-16 16:08:27 +0000

    ScoreEntry.java + ScoreList.java

---
+++ trunk/src/com/googlecode/btuswphalma/gameengine/ScoreList.java
@@ -0,0 +1,11 @@
+package com.googlecode.btuswphalma.gameengine;
+
+public class ScoreList {
+
+	private ScoreEntry scoreEntry;
+	
+	public ScoreList() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
