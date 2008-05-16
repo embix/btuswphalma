e1cc4ad1260770fe369e3ba390617d4ef790103f
chke85 <chke85@1e8899dc-0e4b-0410-8c4f-1f3d18b41dae>
2008-05-16 23:29:11 +0000

    erste Version Board

--- trunk/src/com/googlecode/btuswphalma/gameengine/Board.java
+++ trunk/src/com/googlecode/btuswphalma/gameengine/Board.java
@@ -1,12 +1,58 @@
-/**
- * 
- */
 package com.googlecode.btuswphalma.gameengine;
 
 /**
- * @author sebastian
- *
+ * Das Board (Spielbrett) enthaelt alle Spielfeldpositionen 
+ * und die Positionen aller Spielfeldsteine
+ * @author Christoph
  */
 public class Board {
 
+	/** 2D Array für das Spielfeld (16 Reihen X 12 Zeichen) */
+	private byte[][] boardArray;
+	
+	/**
+	 * Konstruktor der, in Abhaengigkeit von der Spielerzahl,
+	 * das Spielfeld initialisiert 
+	 * @param num int (Spieleranzahl)
+	 */
+	public Board(int num) {
+		this.boardArray = new byte[17][13];
+		//markUsablePositions();
+		//fillWithPlayers(num);
+	}
+
+	/**
+	 * gibt eine Kopie des aktuellen Spielbrettes aus
+	 * @return 	byte[][]  (Spielbrett)
+	 */
+	public byte[][] getBoardArryClone() {
+		byte[][] clone = this.boardArray;
+		return clone;
+	}
+	
+	/**
+	 * gibt den Status einer Spielfeldposition aus
+	 * @param pos BoardPosition (byte, byte)
+	 * @return byte 
+	 */
+	public byte getPositionState(BoardPosition pos) {
+		byte x = pos.getXPos();
+		byte y = pos.getYPos();
+		return this.boardArray[x][y];
+	}
+	
+	/**
+	 * setz den Wert von Board(spos) nach Board(epos) und loescht Board(spos)
+	 * @param spos BoardPosition (byte, byte)
+	 * @param epos BoardPosition (byte, byte)
+	 */
+	public void exchangePositionState(BoardPosition spos, BoardPosition epos) {
+		byte xs = spos.getXPos();
+		byte ys = spos.getYPos();
+		byte xe = epos.getXPos();
+		byte ye = epos.getYPos();
+		byte toChange = this.boardArray[xs][ys];
+		this.boardArray[xe][ye] = toChange;
+		this.boardArray[xs][ys] = 0;
+	}
 }
