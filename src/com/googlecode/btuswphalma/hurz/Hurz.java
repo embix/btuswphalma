f43c3d6bba7448c6f2251eb53856ea4ca8b663a2
michelbretschneider <michelbretschneider@1e8899dc-0e4b-0410-8c4f-1f3d18b41dae>
2008-05-16 13:44:48 +0000

    Konflikt test

--- src/com/googlecode/btuswphalma/hurz/Hurz.java
+++ src/com/googlecode/btuswphalma/hurz/Hurz.java
@@ -3,10 +3,19 @@
  */
 package com.googlecode.btuswphalma.hurz;
 
+
 /**
  * @author sebastian
+ * @author embix
  * Diese Klasse dient dazu, zu testen, ob subclipse richtig arbeitet.
+ * und ob Konflikte beseitigen auch geht.
  */
 public class Hurz {
 
+	/**
+	 * @param args
+	 */
+	public static void main(final String[] args) {
+		System.out.println("Hurz");
+	}
 }
