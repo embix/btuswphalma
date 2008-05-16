50c9f8a5272bf8b18e73d857c0ce2b0c0a49af72
michelbretschneider <michelbretschneider@1e8899dc-0e4b-0410-8c4f-1f3d18b41dae>
2008-05-16 15:30:12 +0000

    +tests Zweig +(Test)PresentationStarter Instanzierung der Presentationsklasse

---
+++ tests/com/googlecode/btuswphalma/gui/PresentationStarter.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.googlecode.btuswphalma.gui;
+
+/**
+ * @author embix
+ *
+ */
+public class PresentationStarter {
+
+    /**
+     * Instanziert ein Presentation
+     * @param args
+     */
+    public static void main(String[] args) {
+	Presentation pres = new Presentation();
+    }
+
+}
