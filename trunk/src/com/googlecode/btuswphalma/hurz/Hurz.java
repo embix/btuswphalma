681df0c7d02d5c69c522ce42eddd67a5ea5cba4c
schmerzkeks <schmerzkeks@1e8899dc-0e4b-0410-8c4f-1f3d18b41dae>
2008-05-16 13:32:52 +0000

    Dies ist lediglich eine Klasse, mit der ich subclipse "teste"

---
+++ trunk/src/com/googlecode/btuswphalma/hurz/Hurz.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.googlecode.btuswphalma.hurz;
+
+/**
+ * @author sebastian
+ * Diese Klasse dient dazu, zu testen, ob subclipse richtig arbeitet.
+ */
+public class Hurz {
+
+}
