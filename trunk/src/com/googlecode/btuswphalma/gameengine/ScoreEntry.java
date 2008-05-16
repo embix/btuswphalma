97ff2140255eff4a048516b6ca59313e580c5a90
chke85 <chke85@1e8899dc-0e4b-0410-8c4f-1f3d18b41dae>
2008-05-16 17:29:16 +0000

    version 08-05-16_19:28

--- trunk/src/com/googlecode/btuswphalma/gameengine/ScoreEntry.java
+++ trunk/src/com/googlecode/btuswphalma/gameengine/ScoreEntry.java
@@ -1,3 +1,8 @@
+/** 
+ * ScoreEntry.java
+ * Beinhaltet einen Spielwertungseintrag (score entry) mit den Werten:
+ * Spielername, Spielerwertung (Rang) und benoetigte Runden
+ */
 package com.googlecode.btuswphalma.gameengine;
 
 public class ScoreEntry {
@@ -5,25 +10,31 @@ public class ScoreEntry {
 	private	int ranking;
 	private	String name;
 	private	int rounds;
-
+	
+	/* der Konstruktor ScoreEntry der mit festen Parametern initialisiert wird */
 	public ScoreEntry(int rank, String spname, int runden) {
 		this.ranking = rank;
 		this.name = spname;
 		this.rounds = runden;
 	}
 	
-	public int ranking() {
+	/* Ausgabe des Ranges dieses Eintrages */
+	public int getRanking() {
 		return this.ranking;
 	}
 	
-	public String name() {
+	/* Ausgabe des Spielernamens dieses Eintrages */
+	public String getName() {
 		return this.name;
 	}
 	
-	public int rounds() {
+	/* Ausgabe der Runden die der Spieler gebraucht hat (aktueller Eintrag) */
+	public int getRounds() {
 		return this.rounds;
 	}
 	
+	/* Setz-Methoden die den aktuellen Eintrag nach dessen Erstellung veraendern koennen
+	 * Verwendung wahrscheinlich nicht noetig
 	public void setRanking(int rank) {
 		this.ranking = rank;
 	}
@@ -34,5 +45,5 @@ public class ScoreEntry {
 	
 	public void setRounds(int runden) {
 		this.rounds = runden;
-	}
+	}*/
 }
