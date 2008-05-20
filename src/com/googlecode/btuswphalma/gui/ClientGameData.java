31f6a93fce476e5f6272bf962729265a63eb1fde
michelbretschneider <michelbretschneider@1e8899dc-0e4b-0410-8c4f-1f3d18b41dae>
2008-05-20 20:12:57 +0000

    +(GUI)ClientGameData: ungekapselt, fuer Spielernamen der Clients +(GUI)DialogClientGameData: Eingabedialog fuer ClientGameData +(GUI-TEST)DialogClientGameDataStarter: Starter, fuer optische Begutachtung und nichtautomatische Tests

---
+++ src/com/googlecode/btuswphalma/gui/ClientGameData.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.googlecode.btuswphalma.gui;
+
+/**
+ * ClientGameData kapselt die Daten der Spieler, die nicht das Spiel leiten,
+ * welche zum Spielbeginn benoetigt werden. Das Objekt ist vorerst nicht
+ * gekapselt, da es momentan noch nicht sinnvoll erscheint.
+ * 
+ * @author embix
+ *
+ */
+public class ClientGameData {
+
+    /**
+     * Name des Spielers
+     */
+    public String playerName;
+
+}
