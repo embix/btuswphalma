92912b79e8d775e5cb52f82b7d0fc0ef47615306
michelbretschneider <michelbretschneider@1e8899dc-0e4b-0410-8c4f-1f3d18b41dae>
2008-05-21 22:32:39 +0000

    > NICHT AUSCHECKEN <

---
+++ trunk/src/com/googlecode/btuswphalma/hurz/ScoreList.java
@@ -0,0 +1,79 @@
+package com.googlecode.btuswphalma.gameengine;
+
+import java.util.ArrayList;
+
+/**
+ * Beinhaltet die Spielerwertungsliste, die aus bis zu 6 Eintraegen besteht
+ * @author Christoph
+ */
+public class ScoreList {
+
+	/**
+	 * ArrayList(ScoreEntry)
+	 */
+	private ArrayList<ScoreEntry> scores = new ArrayList<ScoreEntry>();
+
+	/**
+	 * Konstruktor, Anzahl der Spieler bestimmt Feldgroe�e
+	 * @param num
+	 *            int (Spieleranzahl)
+	 *
+	public ScoreList(int num) {
+	this.scores = new ArrayList<ScoreEntry>(num);
+	}*/
+
+	/**
+	 * leerer Konstruktor
+	 */
+	public ScoreList() { }
+	
+	/**
+	 * gibt die komplette Spielerwertungsliste aus
+	 * @return ArrayList(ScoreEntry)
+	 */
+	public ArrayList<ScoreEntry> getScoreList() {
+	return this.scores;
+	}
+
+	/**
+	 * gibt einen Eintrag, anhand der Position im Feld, aus
+	 * @param rank
+	 *            int
+	 * @return ScoreEntry 
+	 */
+	public ScoreEntry getEntry(int rank) {
+	return scores.get(rank - 1);
+	}
+
+	/**
+	 * fuegt einen Eintrag in das Feld ein, Rang bestimmt dabei dessen Position
+	 * im Feld
+	 * @param entry ScoreEntry
+	 */
+	public void addEntry(ScoreEntry entry) {
+	int pos = entry.getRanking() - 1;
+	if (pos < scores.size()) {
+		this.scores.remove(pos);
+		this.scores.add(pos, entry);
+	} else {
+		entry.setRanking(scores.size());
+		this.scores.add(entry);
+	}
+	}
+	
+	/**
+	 * fuegt einen Eintrag an das Ende der Liste
+	 * @param entry ScoreEntry
+	 */
+	public void addEntryToEnd(ScoreEntry entry) {
+	this.scores.add(entry);
+	}
+	
+	/**
+	 * gibt aktuelle Laenge der Liste aus
+	 * @return int
+	 */
+	public int getSize() {
+	return (this.scores.size());
+	}
+}
