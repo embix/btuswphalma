6801fe72667298364189927edc9946fff010f5b4
michelbretschneider <michelbretschneider@1e8899dc-0e4b-0410-8c4f-1f3d18b41dae>
2008-05-27 19:59:58 +0000

    +(GUI)GuiController: Steuerklasse fuer die Gui (nur Grundgeruest) +(GUI)BuilderHalmaMove: Hilfsklasse zur Erzeugung der Spielzuege (nur Grundgeruest) +(GUI)Inputhandler: Hilfsklasse fuer die Behandlung der Benutzereingaben (nur Grundgeruest) +(GUI)MessageHandler: Objekt zur Nachrichtenbehandlung, Schnittstelle zu den anderen Komponenten(nur Grundgeruest)

---
+++ trunk/src/com/googlecode/btuswphalma/gui/MessageHandler.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.googlecode.btuswphalma.gui;
+
+/**
+ * Die Klasse MessageHandler stellt die wesentliche Schnittstelle nach
+ * aussen (zur Spielengine, evtl. ueber das Netzwerk) dar und dient
+ * dem Austausch der Nachrichten.
+ * 
+ * @author embix
+ *
+ */
+public class MessageHandler {
+
+    // TODO: (GUI) Message Handling implementieren
+}
