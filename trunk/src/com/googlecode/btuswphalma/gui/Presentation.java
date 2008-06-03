bfed3d10bd3bab3c22f96b7da85f847475c808e9
michelbretschneider <michelbretschneider@1e8899dc-0e4b-0410-8c4f-1f3d18b41dae>
2008-06-03 18:14:55 +0000

    (GUI)GuiController: +Methodenrumpf fuer Initialisierung der Praesentation

--- trunk/src/com/googlecode/btuswphalma/gui/Presentation.java
+++ trunk/src/com/googlecode/btuswphalma/gui/Presentation.java
@@ -3,6 +3,8 @@
  */
 package com.googlecode.btuswphalma.gui;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
 
 /**
@@ -17,14 +19,20 @@ public class Presentation extends JFrame {
      */
     private static final long serialVersionUID = 8544934143314222146L;
 
+    private GuiController controller;
+    
     /**
      * Initialisierung
      */
     public Presentation() {
 	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+	this.setLayout(new BorderLayout());
+	
 	this.setSize(640, 480);
 	this.setVisible(true);
+	
+	controller = GuiController.getInstance();
+	controller.initialize(this);
     }
 
 }
