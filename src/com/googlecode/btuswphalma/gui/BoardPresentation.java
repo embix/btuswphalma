c53dcb2e139fc68d7f02c9e0440f367f652b9579
michelbretschneider <michelbretschneider@1e8899dc-0e4b-0410-8c4f-1f3d18b41dae>
2008-06-05 11:09:07 +0000

    (GUI)BoardPresentation: fordert optimale Groesse an

--- src/com/googlecode/btuswphalma/gui/BoardPresentation.java
+++ src/com/googlecode/btuswphalma/gui/BoardPresentation.java
@@ -4,6 +4,7 @@
 package com.googlecode.btuswphalma.gui;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Polygon;
@@ -38,12 +39,14 @@ public class BoardPresentation extends JPanel {
     private HalmaMove move;
     private boolean boardShow; // soll die Belegung gezeichnet werden?
     private boolean moveShow; // soll der aktuelle Zug gezeichnet werden?
-    
+
     /**
      * Konstruktor
      */
     public BoardPresentation() {
-
+	// Anfordern der optimalen Groesse
+	Dimension preferredSize = new Dimension(28 * RADIUS, 36 * RADIUS);
+	this.setPreferredSize(preferredSize);
     }
 
     /**
@@ -64,11 +67,11 @@ public class BoardPresentation extends JPanel {
 	zeichneLeeresBrett(g2d);
 
 	// erst das alte Spielbrett zeigen
-	if(boardShow){
+	if (boardShow) {
 	    zeichneBoard(g2d);
 	}
 	// dann den aktuellen Zug darueberlegen
-	if(moveShow){
+	if (moveShow) {
 	    zeichneMove(g2d);
 	}
     }
@@ -225,24 +228,23 @@ public class BoardPresentation extends JPanel {
     }
 
     /**
-     * Mit dieser Methode wird der Darstellung die neue Spielsituation
-     * bekannt gemacht.
+     * Mit dieser Methode wird der Darstellung die neue Spielsituation bekannt
+     * gemacht.
      * 
      * @param b
      *                legt das aktuelle Spielfeld fest
      */
-    public void setBoard(Board b){
+    public void setBoard(Board b) {
 	this.board = b;
     }
 
     /**
-     * Mit dieser Methode wird der Darstellung der aktuelle Zug bekannt
-     * gemacht.
+     * Mit dieser Methode wird der Darstellung der aktuelle Zug bekannt gemacht.
      * 
      * @param hm
      *                legt den aktuellen Spielzug fest
      */
-    public void setHalmaMove(HalmaMove hm){
+    public void setHalmaMove(HalmaMove hm) {
 	this.move = hm;
     }
 
@@ -253,46 +255,46 @@ public class BoardPresentation extends JPanel {
 	boardShow = true;
 	repaint();
     }
-    
+
     /**
-     * Anhand des Spielbrettes werden die Spielsteine in
-     * der Spielerfarbe zur Anzeige gebracht.
+     * Anhand des Spielbrettes werden die Spielsteine in der Spielerfarbe zur
+     * Anzeige gebracht.
      * 
      * @param g2d
      *                gibt an, in welches Graphikobjekt gezeichnet werden soll
      */
-    private void zeichneBoard(Graphics2D g2d){
-	if(board == null){
+    private void zeichneBoard(Graphics2D g2d) {
+	if (board == null) {
 	    return; // kein Brett heisst nichts zu zeichnen
 	}
 	// TODO: (GUI) unsauber, besser mit getPositionState arbeiten
 	byte boardArray[][] = board.getBoardArryClone();
-	
+
 	// ungeprueftes Anzeigen!
 	// zeilenweise durchgehen
-	for(int i = 0; i <= 16; i++){
-	    for(int j = 0; j <= 12; j++){
-		if(boardArray[i][j] > 0 && boardArray[i][j] < 7){
+	for (int i = 0; i <= 16; i++) {
+	    for (int j = 0; j <= 12; j++) {
+		if (boardArray[i][j] > 0 && boardArray[i][j] < 7) {
 		    // in Board-Klasse:(y)(x)
 		    // Aufruf jedoch (x)(y)
-		    zeichneSpielstein(g2d, j, i, mapPlayerToColor(boardArray[i][j]));
+		    zeichneSpielstein(g2d, j, i,
+			    mapPlayerToColor(boardArray[i][j]));
 		}
 	    }
 	}
     }
-    
+
     /**
-     * Gibt zu einer Spielernummer die entsprechende Farbe
-     * zurueck.
-     * Falls diese Funktion mehrfach benoetigt werden sollte,
-     * ist sie public zu machen und ggf in eine allgemein
-     * zugaengliche GUI-Klasse auszulagern.
+     * Gibt zu einer Spielernummer die entsprechende Farbe zurueck. Falls diese
+     * Funktion mehrfach benoetigt werden sollte, ist sie public zu machen und
+     * ggf in eine allgemein zugaengliche GUI-Klasse auszulagern.
      * 
-     * @param player Spielernummer
+     * @param player
+     *                Spielernummer
      * @return Farbe des Spielers
      */
-    private Color mapPlayerToColor(byte player){
-	switch(player){
+    private Color mapPlayerToColor(byte player) {
+	switch (player) {
 	case 1:
 	    return Color.RED;
 	case 2:
@@ -309,45 +311,48 @@ public class BoardPresentation extends JPanel {
 	    return Color.WHITE;
 	}
     }
-    
+
     /**
      * Der aktuelle Zug wird zur Anzeige gebracht.
      * 
      * @param g2d
      *                gibt an, in welches Graphikobjekt gezeichnet werden soll
      */
-    private void zeichneMove(Graphics2D g2d){
-	if(move == null){
+    private void zeichneMove(Graphics2D g2d) {
+	if (move == null) {
 	    return; // kein Zug heisst nichts zu zeichnen
 	}
-	
+
 	// Postion in Boardbasis
 	BoardPosition vonPos;
 	BoardPosition nachPos;
-	
+
 	// Zielwerte fuer die Rasterpunkte
 	int xVon;
 	int yVon;
 	int xNach;
 	int yNach;
-	for(int i = 0; i < move.getNumberOfPartMoves(); i++){
+	for (int i = 0; i < move.getNumberOfPartMoves(); i++) {
 	    vonPos = move.getPartPosition(i);
-	    nachPos = move.getPartPosition(i+1); // wird immer-1 doppelt ausgelesen
-	    
+	    nachPos = move.getPartPosition(i + 1); // wird immer-1 doppelt
+						    // ausgelesen
+
 	    // Umrechnung Boardbasis in Rasterpunkte
 	    xVon = gibRasterX(vonPos.getXPos(), vonPos.getYPos());
-	    yVon = gibRasterY(vonPos.getYPos()); // doppelt ausgelesen, Performance?
-	    xNach = gibRasterX(nachPos.getXPos(),nachPos.getYPos());
-	    yNach = gibRasterY(nachPos.getYPos()); // doppelt auch ausgelesen, Performance?
-	    
+	    yVon = gibRasterY(vonPos.getYPos()); // doppelt ausgelesen,
+						    // Performance?
+	    xNach = gibRasterX(nachPos.getXPos(), nachPos.getYPos());
+	    yNach = gibRasterY(nachPos.getYPos()); // doppelt auch ausgelesen,
+						    // Performance?
+
 	    // ein Pfeil waere wohl schoener, kann man ja spaeter noch machen
 	    zeichneLinie(g2d, xVon, yVon, xNach, yNach);
 	}
     }
-    
+
     /**
-     * Zeichne einen farbigen Spielstein enstrechend der Position zur
-     * Basis des Board Objektes. Diese entspricht nicht dem Graphikraster!
+     * Zeichne einen farbigen Spielstein enstrechend der Position zur Basis des
+     * Board Objektes. Diese entspricht nicht dem Graphikraster!
      * 
      * @param g2d
      *                gibt an, in welches Graphikobjekt gezeichnet werden soll
@@ -358,43 +363,48 @@ public class BoardPresentation extends JPanel {
      * @param c
      *                gibt die zu verwendende Farbe an
      */
-    private void zeichneSpielstein(Graphics2D g2d, int xPos, int yPos, Color c){
+    private void zeichneSpielstein(Graphics2D g2d, int xPos, int yPos, Color c) {
 	// Umrechnung von Board-Basis in Rasterpunkte
-	int x = gibRasterX(xPos,yPos);
+	int x = gibRasterX(xPos, yPos);
 	int y = gibRasterY(yPos);
 
 	// zeichnet Spielstein als kleinen Kreis
 	// kann spaeter auch huebscher umgesetzt werden
-	zeichneKleinenKreis(g2d,x,y,c);
+	zeichneKleinenKreis(g2d, x, y, c);
     }
-    
+
     /**
      * Rechnet Boardpunkte in Rasterkoordinate x um
-     * @param xPos x-Position in Boardbasis
-     * @param yPos y-Position in Boardbasis
+     * 
+     * @param xPos
+     *                x-Position in Boardbasis
+     * @param yPos
+     *                y-Position in Boardbasis
      * @return x-Position im Raster
      */
-    public static int gibRasterX(int xPos, int yPos){
+    public static int gibRasterX(int xPos, int yPos) {
 	int x;
 	// Ungepruefte Umrechnung
-	if(yPos % 2 == 0){
-	    x = 2 + 2*xPos;
-	}else{
-	    x = 3 + 2*xPos;
+	if (yPos % 2 == 0) {
+	    x = 2 + 2 * xPos;
+	} else {
+	    x = 3 + 2 * xPos;
 	}
 	return x;
     }
-    
+
     /**
      * Rechnet Boardkoordiante y in Rasterkoordinate y um
-     * @param yPos y-Position in Boardbasis
+     * 
+     * @param yPos
+     *                y-Position in Boardbasis
      * @return y-Position im Raster
      */
-    public static int gibRasterY(int yPos){
+    public static int gibRasterY(int yPos) {
 	// Ungepruefte Umrechnung
-	return (2 + 2*yPos);
+	return (2 + 2 * yPos);
     }
-    
+
     /**
      * Zeichnen einen kleinen Kreises
      * 
@@ -413,20 +423,25 @@ public class BoardPresentation extends JPanel {
 	int xpos = RADIUS * x;
 	int ypos = RADIUS * y;
 	// Kreismittelpunkt entspricht (xStart-xEnd, yStart-yEnd)
-	g2d.fillOval(xpos - RADIUS/2, ypos - RADIUS/2, RADIUS, RADIUS);
+	g2d.fillOval(xpos - RADIUS / 2, ypos - RADIUS / 2, RADIUS, RADIUS);
     }
-    
 
     /**
      * Zeichnen einer schwarzen Linie
      * 
-     * @param g2d gibt an, in welches Graphikobjekt gezeichnet werden soll
-     * @param xVon gibt den x Wert des Startpunktes im Raster an
-     * @param yVon gibt den y Wert des Startpunktes im Raster an
-     * @param xNach gibt den x Wert des Zielpunktes im Raster an
-     * @param yNach gibt den y Wert des Zielpunktes im Raster an
+     * @param g2d
+     *                gibt an, in welches Graphikobjekt gezeichnet werden soll
+     * @param xVon
+     *                gibt den x Wert des Startpunktes im Raster an
+     * @param yVon
+     *                gibt den y Wert des Startpunktes im Raster an
+     * @param xNach
+     *                gibt den x Wert des Zielpunktes im Raster an
+     * @param yNach
+     *                gibt den y Wert des Zielpunktes im Raster an
      */
-    private void zeichneLinie(Graphics2D g2d, int xVon, int yVon, int xNach, int yNach){
+    private void zeichneLinie(Graphics2D g2d, int xVon, int yVon, int xNach,
+	    int yNach) {
 	g2d.setPaint(Color.BLACK);
 	// Skalierte Boardposition berechnen
 	xVon *= RADIUS;
@@ -436,7 +451,7 @@ public class BoardPresentation extends JPanel {
 	// Zeichnen der Linie
 	g2d.drawLine(xVon, yVon, xNach, yNach);
     }
-    
+
     /**
      * Zeige den aktuellen Zug ueber dem letzten Board auf dem Bildschirm an.
      */
@@ -444,23 +459,23 @@ public class BoardPresentation extends JPanel {
 	moveShow = true;
 	repaint();
     }
-    
+
     /**
-     * Wird aufgerufen, wenn der aktuelle Spielzug nicht
-     * mehr angezeigt werden soll.
-     *
+     * Wird aufgerufen, wenn der aktuelle Spielzug nicht mehr angezeigt werden
+     * soll.
+     * 
      */
-    public void hideMove(){
+    public void hideMove() {
 	moveShow = false;
 	repaint();
     }
-    
+
     /**
-     * Wird aufgerufen, wenn die Position der Spielsteine
-     * nicht mehr angezeigt werden soll.
-     *
+     * Wird aufgerufen, wenn die Position der Spielsteine nicht mehr angezeigt
+     * werden soll.
+     * 
      */
-    public void hideBoard(){
+    public void hideBoard() {
 	boardShow = false;
 	repaint();
     }
