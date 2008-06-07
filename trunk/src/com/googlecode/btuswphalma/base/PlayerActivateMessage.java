4ee992cebeca275e48ab5a22a5a51f5ec7c2838b
schmerzkeks@gmail.com <schmerzkeks@gmail.com@1e8899dc-0e4b-0410-8c4f-1f3d18b41dae>
2008-06-07 14:37:15 +0000

    Die PlayerAcivateMessage kommt dazu

---
+++ trunk/src/com/googlecode/btuswphalma/base/PlayerActivateMessage.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.googlecode.btuswphalma.base;
+
+/**
+ * Durch den Empfang einer Nachricht diesen Typs, weiss ein Client
+ * dass jetzt er einen Zug uebermittlen soll.
+ * @author sebastian
+ *
+ */
+public class PlayerActivateMessage extends AbstractMessage {
+
+    /**
+     * @param source die Quelladresse
+     * @param destination die Zieladresse
+     */
+    public PlayerActivateMessage(int source, int destination) {
+	super(source, destination);
+    }
+
+    /* (non-Javadoc)
+     * @see com.googlecode.btuswphalma.base.IMessage#getType()
+     */
+    @Override
+    public MessageType getType() {
+	return MessageType.MT_PLAYERACTIVATE;
+    }
+
+}
