00e932355be5ebed940cc3154702c7219b5b0dce
schmerzkeks <schmerzkeks@1e8899dc-0e4b-0410-8c4f-1f3d18b41dae>
2008-06-18 23:43:24 +0000

    Kommentare

--- tests/com/googlecode/btuswphalma/gameengine/HalmaMathTest.java
+++ tests/com/googlecode/btuswphalma/gameengine/HalmaMathTest.java
@@ -3,9 +3,8 @@
  */
 package com.googlecode.btuswphalma.gameengine;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 import junit.framework.Assert;
-import junit.framework.TestCase;
 
 import org.junit.Ignore;
 import org.junit.Test;
@@ -15,8 +14,10 @@ import org.junit.Test;
  * 
  */
 public class HalmaMathTest {
-    
-    // Per Hand eingegeben, die korrekten isOnBoard Antworten
+
+    /**
+     * Per Hand eingegeben, die korrekten isOnBoard Antworten
+     */
     private boolean[][] correctBoardAnswers ={
 	    {false, false, false, false, false, false,  true, false, false, false, false, false, false},
 	    {false, false, false, false, false,  true,  true, false, false, false, false, false, false},
@@ -36,10 +37,10 @@ public class HalmaMathTest {
 	    {false, false, false, false, false,  true,  true, false, false, false, false, false, false},
 	    {false, false, false, false, false, false,  true, false, false, false, false, false, false}
     };
-     
-     
-
     /**
+     * Fuer alle Moeglichen Werte, die ein Byte haben kann, wird die Methode
+     * getestet (by byte noch Sinnvoll)
+     * 
      * Test method for
      * {@link com.googlecode.btuswphalma.gameengine.HalmaMath#isOnBoard(com.googlecode.btuswphalma.gameengine.BoardPosition)}.
      */
@@ -47,12 +48,13 @@ public class HalmaMathTest {
     public void testIsOnBoardBoardPosition() {
 	for (int x = 0; x < correctBoardAnswers.length; x++) {
 	    for (int y = 0; y < correctBoardAnswers[1].length; y++) {
-		Assert.assertEquals(correctBoardAnswers[x][y],HalmaMath.isOnBoard((byte)x, (byte)y));
+		Assert.assertEquals(correctBoardAnswers[x][y], HalmaMath
+			.isOnBoard((byte) x, (byte) y));
 	    }
 	}
 	for (byte x = -128; x < 127; x++) {
 	    for (byte y = -128; y < 127; y++) {
-		if(x<0||x>16||y<0||y>12) {
+		if (x < 0 || x > 16 || y < 0 || y > 12) {
 		    Assert.assertEquals(false, HalmaMath.isOnBoard(x, y));
 		}
 	    }
@@ -60,40 +62,51 @@ public class HalmaMathTest {
     }
 
     /**
-     * Test method for
+     * Der Test wird noch ausgelassen, ist aber nicht sehr spannend Test method
+     * for
      * {@link com.googlecode.btuswphalma.gameengine.HalmaMath#isOnBoard(byte, byte)}.
      */
     @Test
     @Ignore
     public void testIsOnBoardByteByte() {
-	
+
 	fail("Not yet implemented");
     }
 
     /**
-     * Test method for
+     * Fuer alle Boardpositionen als werden alle moeglichen Ziele getestet. Der
+     * Test ist Fraglich, da die Formel zum ermitteln der richtigen Antwort wohl
+     * der selbe wie in der getesteten Methode ist. Zwat wurde sie nicht
+     * uebernommen, aber von der selben Person erstellt. Test method for
      * {@link com.googlecode.btuswphalma.gameengine.HalmaMath#isPushDistance(com.googlecode.btuswphalma.gameengine.BoardPosition, com.googlecode.btuswphalma.gameengine.BoardPosition)}.
      */
     @Test
     public void testIsPushDistance() {
-	// Fuer jede potentielle Spielbrettposition (innerhalb des 17*13 Gebiets) werden alle moeglichen
+	// Fuer jede potentielle Spielbrettposition (innerhalb des 17*13
+	// Gebiets) werden alle moeglichen
 	// Endpositionen geprueft
 	BoardPosition startPos = null;
 	BoardPosition endPos = null;
 	boolean res;
 	for (byte x = -128; x < 127; x++) {
 	    for (byte y = -128; y < 127; y++) {
-		endPos = new BoardPosition(x,y);
+		endPos = new BoardPosition(x, y);
 		for (byte xs = 0; xs <= 16; xs++) {
 		    for (byte ys = 0; ys <= 12; ys++) {
-			startPos = new BoardPosition(xs,ys);
-			res =        ((x == xs-1 || x == xs+1) && (xs%2==0) && (y == ys-1 || y == ys));
-			res = res || ((x == xs-1 || x == xs+1) && (xs%2==1) && (y == ys || y == ys+1));
-			res = res || (x==xs &&(y==ys-1||y==ys+1));
-			Assert.assertEquals(res, HalmaMath.isPushDistance(startPos, endPos));
-			Assert.assertEquals(false, HalmaMath.isPushDistance(startPos, null));
-			Assert.assertEquals(false, HalmaMath.isPushDistance(null, endPos));
-			Assert.assertEquals(false, HalmaMath.isPushDistance(null, null));
+			startPos = new BoardPosition(xs, ys);
+			res = ((x == xs - 1 || x == xs + 1) && (xs % 2 == 0) && (y == ys - 1 || y == ys));
+			res = res
+				|| ((x == xs - 1 || x == xs + 1)
+					&& (xs % 2 == 1) && (y == ys || y == ys + 1));
+			res = res || (x == xs && (y == ys - 1 || y == ys + 1));
+			Assert.assertEquals(res, HalmaMath.isPushDistance(
+				startPos, endPos));
+			Assert.assertEquals(false, HalmaMath.isPushDistance(
+				startPos, null));
+			Assert.assertEquals(false, HalmaMath.isPushDistance(
+				null, endPos));
+			Assert.assertEquals(false, HalmaMath.isPushDistance(
+				null, null));
 		    }
 		}
 	    }
@@ -101,28 +114,36 @@ public class HalmaMathTest {
     }
 
     /**
-     * Test method for
+     * Fuer alle Boardpositionen als werden alle moeglichen Ziele getestet. Der
+     * Test ist Fraglich, da die Formel zum ermitteln der richtigen Antwort wohl
+     * der selbe wie in der getesteten Methode ist. Zwat wurde sie nicht
+     * uebernommen, aber von der selben Person erstellt. Test method for
      * {@link com.googlecode.btuswphalma.gameengine.HalmaMath#isJumpDistance(com.googlecode.btuswphalma.gameengine.BoardPosition, com.googlecode.btuswphalma.gameengine.BoardPosition)}.
      */
     @Test
     public void testIsJumpDistance() {
-	// Fuer jede potentielle Spielbrettposition (innerhalb des 17*13 Gebiets) werden alle moeglichen
+	// Fuer jede potentielle Spielbrettposition (innerhalb des 17*13
+	// Gebiets) werden alle moeglichen
 	// Endpositionen geprueft
 	BoardPosition startPos = null;
 	BoardPosition endPos = null;
 	boolean res;
 	for (byte x = -128; x < 127; x++) {
 	    for (byte y = -128; y < 127; y++) {
-		endPos = new BoardPosition(x,y);
+		endPos = new BoardPosition(x, y);
 		for (byte xs = 0; xs <= 16; xs++) {
 		    for (byte ys = 0; ys <= 12; ys++) {
-			startPos = new BoardPosition(xs,ys);
-			res =        ((x == xs-2 || x == xs+2) && (y == ys-1 || y == ys+1));
-			res = res || (x==xs &&(y==ys-2||y==ys+2));
-			Assert.assertEquals(res, HalmaMath.isJumpDistance(startPos, endPos));
-			Assert.assertEquals(false, HalmaMath.isJumpDistance(startPos, null));
-			Assert.assertEquals(false, HalmaMath.isJumpDistance(null, endPos));
-			Assert.assertEquals(false, HalmaMath.isJumpDistance(null, null));
+			startPos = new BoardPosition(xs, ys);
+			res = ((x == xs - 2 || x == xs + 2) && (y == ys - 1 || y == ys + 1));
+			res = res || (x == xs && (y == ys - 2 || y == ys + 2));
+			Assert.assertEquals(res, HalmaMath.isJumpDistance(
+				startPos, endPos));
+			Assert.assertEquals(false, HalmaMath.isJumpDistance(
+				startPos, null));
+			Assert.assertEquals(false, HalmaMath.isJumpDistance(
+				null, endPos));
+			Assert.assertEquals(false, HalmaMath.isJumpDistance(
+				null, null));
 		    }
 		}
 	    }
