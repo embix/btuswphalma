edc50f6edf6d7910d9c39cbe4264a563565fb3b5
chke85 <chke85@1e8899dc-0e4b-0410-8c4f-1f3d18b41dae>
2008-06-19 19:44:16 +0000

    getestete Änderungen in FinishChecker

--- trunk/tests/com/googlecode/btuswphalma/gameengine/TestFinishChecker.java
+++ trunk/tests/com/googlecode/btuswphalma/gameengine/TestFinishChecker.java
@@ -13,6 +13,8 @@ public class TestFinishChecker {
      */
     public static void main(String[] args) {
 
+    FinishChecker check = new FinishChecker();
+    
 	PlayerList players = new PlayerList();
 
 	for (int i = 1; i < 7; i++) {
@@ -25,10 +27,26 @@ public class TestFinishChecker {
 	    play.setSpec(true);
 	}
 
-	FinishChecker check = new FinishChecker();
+	boolean xx = check.checkGameFinished(players);
+	System.out.println(String.valueOf(xx));
 
-	boolean x = check.checkGameFinished(players);
-	System.out.println(String.valueOf(x));
+    int num = 6;
+    int pl = 2;
+    byte xs = 0; byte ys = 6;
+    byte xe = 7; byte ye = 6;
+    Board board = new Board(num);
+    BoardPosition spos = new BoardPosition(xs, ys);
+    BoardPosition epos = new BoardPosition(xe, ye);
+    board.exchangePositionState(spos, epos);
+    
+    boolean cp = check.checkPlayerCompleted(board, pl, num);
+    System.out.println(String.valueOf(cp));
+    
+    /** fuer folgende tests auf public methods abaendern
+    int le = check.locateEndHouse(pl, num);
+    System.out.println(le);
+    
+    boolean pf = check.playerIsFinished(board, pl, pl);
+    System.out.println(pf);*/
     }
-
 }
