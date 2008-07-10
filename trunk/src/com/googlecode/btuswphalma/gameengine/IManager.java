ee34c67c1b7e3edac24c2c5f40003bc1062af23e
schmerzkeks <schmerzkeks@1e8899dc-0e4b-0410-8c4f-1f3d18b41dae>
2008-07-10 15:58:06 +0000

    Form

--- trunk/src/com/googlecode/btuswphalma/gameengine/IManager.java
+++ trunk/src/com/googlecode/btuswphalma/gameengine/IManager.java
@@ -8,8 +8,8 @@ import com.googlecode.btuswphalma.base.IMessage;
 /**
  * Eine Implementierung des IManagers dient dazu, das Spiel auszüfuhren,
  * inklusive der zeitlichen Steuerung. Das Interface dient auch dazu, dass die
- * Implementierung von SpieleEngine und die Servergine nicht aufeinander warten
- * müssen
+ * Implementierung von SpieleEngine und die Serverengine nicht aufeinander warten
+ * muessen
  * 
  * @author sebastian
  * 
@@ -17,7 +17,7 @@ import com.googlecode.btuswphalma.base.IMessage;
 public interface IManager {
 
     /**
-     * Es wird eine Nachricht übergeben. Dabei wird aber nicht gewährleistet,
+     * Es wird eine Nachricht uebergeben. Dabei wird aber nicht gewährleistet,
      * wann die Nachricht verarbeitet wird. Typischerweise landet sie auf einer
      * Queue.
      * 
